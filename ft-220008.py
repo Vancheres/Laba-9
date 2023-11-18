416db9a0df07dc36012d0f6811613becfe66cfe8
Vancheres <anisii840@gmail.com>
2023-11-18 06:04:55 +0500

    radom and logg

--- ft-220008.py
+++ ft-220008.py
@@ -3,3 +3,12 @@ import logging
 
 # Настройка логирования
 logging.basicConfig(filename='lottery_log.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+def draw_barrel(bag):
+    try:
+        drawn_barrel = random.choice(bag)
+        bag.remove(drawn_barrel)
+        logging.info(f"Бочонок {drawn_barrel} был вытянут.")
+        return drawn_barrel
+    except IndexError:
+        logging.error("Все бочонки были вытянуты.")
+        return None
